e429164abaa6a71febe2f4c728dc8c4b63be2549
laarnisb <laarnisb49@gmail.com>
2025-05-27 01:45:34 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
+import streamlit as st
 from sqlalchemy import create_engine
-import os
 
-DB_USER = os.getenv("DB_USER", "yourusername")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "yourpassword")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "wisebudget")
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_HOST = st.secrets["DB_HOST"]
+DB_PORT = st.secrets["DB_PORT"]
+DB_NAME = st.secrets["DB_NAME"]
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DATABASE_URL)
